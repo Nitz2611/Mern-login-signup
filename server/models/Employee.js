8890c112d4b30d116ea977fb88dbcf831f15576c
Nitish sharma <sharmanitish183@gmail.com>
2023-12-17 18:52:42 +0530

    first commit

---
+++ server/models/Employee.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const EmployeeSchema = new mongoose.Schema({
+    name: String,
+    email: { type: String, unique: true },
+    password: String
+})
+
+const EmployeeModel = mongoose.model("employees", EmployeeSchema);
+module.exports = EmployeeModel
